90f4983375ef98e946de5d004ddf10466c72558b
tomekstanczak <tomek.stanczak87@gmail.com>
2024-06-05 22:49:04 +0200

    Routes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,7 +7,9 @@ export const Navigation = () => {
   return (
     <nav className={css.navStyle}>
       <NavLink to="/goit-react-hw-08-phonebook">Home</NavLink>
-      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+      {isLoggedIn && (
+        <NavLink to="/goit-react-hw-08-phonebook/contacts">Contacts</NavLink>
+      )}
       {!isLoggedIn && (
         <>
           <NavLink to="/goit-react-hw-08-phonebook/register">Register</NavLink>
